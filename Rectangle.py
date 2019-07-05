b86b7da3d8ca7acd57ab69866b45f9f2c6dfeb58
qwertyoleg <olegpetrushov@gmail.com>
2019-07-05 10:47:37 +0300

    added plus operation

--- Rectangle.py
+++ Rectangle.py
@@ -6,6 +6,25 @@ class Rectangle:
         self.__height = height
         self.__colour = colour
         self.__width = width
+
+
+    def get_colour(self):
+        return self.__colour
+
+
+    def get_width(self):
+        return self.__width
+
+
+    def get_height(self):
+        return self.__height
+
+
+    def __add__(self, other):
+        return Rectangle(self.__colour, \
+                    max(float(self.__height),float(other.get_height())), \
+                    max(float(self.__width), float(other.get_width())))
+        self.__colour
     def __str__(self):
         return "Rectangle colour is " + str(self.__colour)\
                + ", width is " + str(self.__width)\
@@ -13,4 +32,8 @@ class Rectangle:
 
 
 r = Rectangle("red", 3, 15)
+r2 = Rectangle("Green", 5, 12)
+r3 = r +r2
 print(r)
+print(r2)
+print(r3)
